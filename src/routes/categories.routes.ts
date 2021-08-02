32307c4f3c85ecf93ce0034253c1c40ddb49286e
x0n4d0 <romagnolimauricio@gmail.com>
2021-08-02 15:03:03 -0300

    feat: validate if category name already exist

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,12 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
+  const hasOneCategoryWithThisName = categoriesRepository.findByName(name);
+
+  if (hasOneCategoryWithThisName) {
+    return response.status(400).json({ error: 'Category Already Exists!' });
+  }
+
   categoriesRepository.create({ name, description });
 
   return response.status(201).send();
