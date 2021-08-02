e5360d4aca8a699fe8bb86f034ea5e05d8b26b74
x0n4d0 <romagnolimauricio@gmail.com>
2021-08-02 12:06:48 -0300

    chore: remove route and add console.log to print when app is running

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
+app.listen(3333, () => {
+  console.log(`[*] Server running!`);
 });
-
-app.listen(3333);
