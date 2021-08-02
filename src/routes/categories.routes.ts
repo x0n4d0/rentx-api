2f248a9c91b9ce4778c7dac3f7fabc720f9dbe01
x0n4d0 <romagnolimauricio@gmail.com>
2021-08-02 18:14:05 -0300

    refactor: add ListCreategoryController to remove route responsibility

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,16 @@
 import { Router } from 'express';
 
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
 import { createCategoryController } from '../modules/cars/usecases/create-category';
+import { listCategoriesController } from '../modules/cars/usecases/list-categories';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get('/', (request, response) => {
-  const categories = categoriesRepository.list();
-
-  return response.json(categories);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
