81858afddb2be08522f3f88598ba7eddd644b60a
x0n4d0 <romagnolimauricio@gmail.com>
2021-08-02 21:50:30 -0300

    chore: fix ICategoriesRepository import path

--- src/modules/cars/usecases/import-category/ImportCategoryUseCase.ts
+++ src/modules/cars/usecases/import-category/ImportCategoryUseCase.ts
@@ -1,7 +1,7 @@
 import csvParse from 'csv-parse';
 import fs from 'fs';
 
-import { ICategoriesRepository } from '../../../../repositories/ICategoriesRepository';
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
 
 interface IImportCategory {
   name: string;
