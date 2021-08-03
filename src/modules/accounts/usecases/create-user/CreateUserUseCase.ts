215c12a6e753c214c1f96fc2d8df3db5d4bd196f
x0n4d0 <romagnolimauricio@gmail.com>
2021-08-03 16:35:43 -0300

    feat: add accounts module to User

---
+++ src/modules/accounts/usecases/create-user/CreateUserUseCase.ts
@@ -0,0 +1,26 @@
+import { inject, injectable } from 'tsyringe';
+
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { UsersRepository } from '../../repositories/implementations/UsersRepository';
+
+@injectable()
+export class CreateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: UsersRepository,
+  ) {}
+
+  async execute({
+    name,
+    password,
+    email,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    await this.usersRepository.create({
+      name,
+      password,
+      email,
+      driver_license,
+    });
+  }
+}
