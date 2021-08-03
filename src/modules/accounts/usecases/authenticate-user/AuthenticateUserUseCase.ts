27b51afecf904b46e36beaf65ec8574565010f63
x0n4d0 <romagnolimauricio@gmail.com>
2021-08-03 17:28:21 -0300

    feat: create authentication with jsonwebtoken

---
+++ src/modules/accounts/usecases/authenticate-user/AuthenticateUserUseCase.ts
@@ -0,0 +1,55 @@
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+export class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error('Email or passord is incorrect!');
+    }
+
+    const isTheSamePassword = await compare(password, user.password);
+
+    if (!isTheSamePassword) {
+      throw new Error('Email or passord is incorrect!');
+    }
+
+    const token = sign({}, '110eff7d40a3779f4513c1be6f8fffcf', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    const responseWithUserInfoAndToken: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return responseWithUserInfoAndToken;
+  }
+}
