1499d3bfa2083a3dfd6d4c4a7de79507d0ae7d49
x0n4d0 <romagnolimauricio@gmail.com>
2021-08-03 17:59:32 -0300

    feat: add express-async-errors and middleware to catch errors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
+
+import { AppError } from './errors/AppError';
 
 import './database';
 
@@ -16,6 +19,21 @@ app.use(express.json());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    });
+  },
+);
 
 app.listen(3333, () => {
   console.log(`[*] Server running!`);
