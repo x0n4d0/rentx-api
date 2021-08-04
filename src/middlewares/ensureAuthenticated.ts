a3e8c7dbc7ce444b6da6521253113684032191ec
x0n4d0 <romagnolimauricio@gmail.com>
2021-08-03 22:21:46 -0300

    feat: implements user avatar upload

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -35,6 +35,8 @@ export async function ensureAuthenticated(
       throw new AppError('User does not exists!', 401);
     }
 
+    request.user = { id: user_id };
+
     next();
   } catch {
     throw new AppError('Invalid token!', 401);
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,25 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '../config/upload';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { CreateUserController } from '../modules/accounts/usecases/create-user/CreateUserController';
+import { UpdateUserAvatarController } from '../modules/accounts/usecases/update-user-avatar/UpdateUserAvatarController';
 
 const usersRoutes = Router();
 
+const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'));
+
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post('/', createUserController.handle);
+
+usersRoutes.patch(
+  '/avatar',
+  ensureAuthenticated,
+  uploadAvatar.single('avatar'),
+  updateUserAvatarController.handle,
+);
 
 export { usersRoutes };
